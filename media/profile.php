795a0013d586d4d6815887f75a0e4035916a2fed
Shafqat Jan <shafqatjan86@gmail.com>
2012-11-13 15:10:24 +0500

    first commitadsfasdfasdfadsfasdf

---
+++ media/profile.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8">
+<title>Publicitus</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<script src="js/modernizr.js"></script>
+<!--[if IE 6]>
+<link href="css/IE/style-IE-6.css" rel="stylesheet" type="text/css">
+<![endif]-->
+
+<!--[if IE 7]>
+<link href="css/IE/style-IE-7.css" rel="stylesheet" type="text/css">
+<![endif]-->
+
+<!--[if IE 8]>
+<link href="css/IE/style-IE-8.css" rel="stylesheet" type="text/css">
+<![endif]-->
+
+</head>
+
+<body>
+
+ <div id="warpper">
+ 
+  <div id="header">
+   <div id="logo">
+    <img src="images/logo.png" />
+   </div> 
+   <div id="nav"> 
+    <ul>
+     <li> <a href="#"> Home </a> </li>
+     <li> <a href="#"> Join </a> </li>
+     <li style="border:none;"> <a href="#"> Sign In </a> </li>
+     <li style="border:none;"> <a href="#"> <img src="images/fb.png" /> </a> </li>
+    </ul>
+   </div>
+  </div>
+  
+  <div id="content">
+  
+   <div id="profile-warrper">
+   
+    <div class="profile-blue-box"> <!-- first blue box -->
+    
+     <div class="profile-blue-first"> <!-- blue box first row -->
+      <span class="profile-blue-heading"> <h3> Does Abdul jabar have these skill or experties? </h3> </span>
+      <span class="profile-blue-close-btn"> x </span>
+     </div> <!-- blue box first row -->
+     
+     <div class="profile-blue-second"> <!-- blue box Second row -->    
+       
+       <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+       <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+        <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+        <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+        <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+        <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+        <div class="profile-blue-second-btn"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> Robotics </span>
+        <span class="profile-blue-second-btn-close"> X </span>
+       </div> <!-- fisrt blue box Skill button --->
+       
+        <div class="profile-blue-second-input"> <!-- fisrt blue box Skill button --->
+        <span class="profile-blue-second-btn-text"> 
+         <input type="text" value="Type an other area of experties" >         
+        </span>
+        
+       </div> <!-- fisrt blue box Skill button --->
+       
+     </div> <!-- blue box second row -->
+     
+     <div class="profile-blue-third"> <!-- blue box third row -->
+     
+      <span class="blue-box-endorse-btn"> <input type="button" value="Endorse"> </span>
+      <span class="blue-box-skip-btn"> <input type="button" value="skip"> </span>
+      <span class="blue-box-what-is-this"> <a href="#"> what is this? </a> </span>
+     
+     </div> <!-- blue box third row -->
+    
+    </div> <!-- first blue box -->
+
+    
+   </div> <!-- profile warrper -->
+
+  
+  </div> <!-- content -->
+  
+  <!-- footer -->
+  <div id="footer">
+   <div id="copyright"> 
+    <p> Copy Right &copy; 2012 ABC Company.ALL rights reserved. </p>
+   </div>
+   <div id="social-icons">
+    <ul id="sn_icons">
+      <li>
+      <a class="facebook" href="Write your Facebook id.">   </a>
+      </li>
+      <li>
+      <a class="twitter" href="Write your Twitter id.">   </a>
+      </li>
+      <li>
+      <a class="linkedin" href="Write your LinkedIn id.">   </a>
+      </li>
+      <li>
+      <a class="google" href="Write your Google+ id.">   </a>
+      </li>
+      <li>
+      <a class="youtube" href="Write your Youtube id.">   </a>
+      </li>
+      <li>
+      <a class="feed" href="Write your RSS Feed id.">   </a>
+      </li>
+    </ul>
+   </div>
+  </div>
+ 
+ </div> <!-- Warpper -->
+
+</body>
+</html>
